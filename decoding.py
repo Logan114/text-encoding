ef2ae3e2af6bc6707567c86b1fe8210d25d7564e
logan114 <kepirobalazs204@gmail.com>
2024-06-17 06:21:43 +0200

    question for the encrypted text file added

--- decoding.py
+++ decoding.py
@@ -1,9 +1,18 @@
+
+print("Do you have an encrypted text in a txt file?")
+key = input().strip().lower()
+if key == 'n':
+    encoded = input("Please paste the encrypted text\n")
+elif key == 'y':
+    filename = input("what is the filename of it?")
+    kiFile = open (filename,"r")
+    encoded = kiFile.read()
+      
 seed = open("seed.txt","r")
-kiFile = open ("out.txt","r")
+
 shift = seed.readlines()
 output = ""
 charcode:int = 0
-encoded = kiFile.read()
 
 for i in range (0,len(encoded)):
     charcode = (ord(encoded[i])) - int(shift[i])
