36d996f323e951d37ec1c0a2910bc6cbe6322bb1
logan114 <kepirobalazs204@gmail.com>
2024-06-17 06:09:25 +0200

    fixed the hunglish

--- decoding.py
+++ decoding.py
@@ -2,10 +2,10 @@ seed = open("seed.txt","r")
 kiFile = open ("out.txt","r")
 shift = seed.readlines()
 output = ""
-charkod:int = 0
-kodolt = kiFile.read()
+charcode:int = 0
+encoded = kiFile.read()
 
-for i in range (0,len(kodolt)):
-    charkod = (ord(kodolt[i])) - int(shift[i])
-    output +=(chr(charkod))
+for i in range (0,len(encoded)):
+    charcode = (ord(encoded[i])) - int(shift[i])
+    output +=(chr(charcode))
 print("The decoded text is:\n",output)
